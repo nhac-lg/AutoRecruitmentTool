a26f53ba6a7f8bcbdedc31f7a757c9b439502927
lethibethuycntt13b <lethuy281019990603@gmail.com>
2021-09-21 10:37:39 +0700

    fix

--- src/main/java/gui/App.java
+++ src/main/java/gui/App.java
@@ -1,14 +1,11 @@
 package gui;
 
-import com.sun.javafx.css.StyleManager;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 
 /**
@@ -18,7 +15,9 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        URL url = new File("src/main/java/gui/page/MainUI.fxml").toURI().toURL();
+        //URL url = new File("src/main/java/gui/page/MainUI.fxml").toURI().toURL();
+        URL url = new File("src/main/java/gui/page/DetailCV.fxml").toURI().toURL();
+        System.out.println(url);
         URL css = new File("src/main/java/gui/App.css").toURI().toURL();
         Parent root = FXMLLoader.load(url);
         primaryStage.setTitle("Candidate management");
--- src/main/java/gui/controller/DetailCV.java
+++ src/main/java/gui/controller/DetailCV.java
@@ -5,9 +5,14 @@
  */
 package gui.controller;
 
+import java.awt.event.MouseEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javax.swing.Icon;
 
 /**
  * FXML Controller class
@@ -19,9 +24,49 @@ public class DetailCV implements Initializable {
     /**
      * Initializes the controller class.
      */
+    @FXML
+    private Label txt_name;
+
+    @FXML
+    private TextField txt_pos;
+
+    @FXML
+    private TextField txt_company;
+
+    @FXML
+    private TextField txt_year;
+
+    @FXML
+    private TextField txt_salary;
+
+    @FXML
+    private TextField txt_srcCV;
+
+    @FXML
+    private TextField txt_job;
+
+    @FXML
+    private TextField txt_location;
+
+    @FXML
+    private TextField txt_gender;
+
+    @FXML
+    private Label text_pos;
+    
+     @FXML
+    private Icon btnClose;
+     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+    @FXML
+    private void handleClose(javafx.scene.input.MouseEvent event){
+        if(event.getSource()==btnClose){
+            System.exit(0);
+        }
+        
+    }
     
 }
